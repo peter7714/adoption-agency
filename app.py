d2aec83a029f2c73f6c0d16ee2b4d0ed00f763fa
peter <peter.26cast@gmail.com>
2023-02-11 20:24:37 -0500

    .

--- app.py
+++ app.py
@@ -1,7 +1,7 @@
 from flask import Flask, request, redirect, render_template
 from flask_debugtoolbar import DebugToolbarExtension 
-from models import db, connect_db, Pet
-from forms import AddPetForm
+from models import db, connect_db, Pet, DEFAULT_IMG
+from forms import AddPetForm, EditPetForm
 
 app = Flask(__name__)
 app.config['SQLALCHEMY_DATABASE_URI'] = 'postgresql:///adoption_agency'
@@ -17,23 +17,23 @@ debug = DebugToolbarExtension(app)
 connect_db(app)
 db.create_all()
 
-if Pet.query.order_by(Pet.name).all() == []:
+if Pet.query.all() == []:
     woofy = Pet(
         name = 'Woofy',
-        species = 'pomchi',
+        species = 'Dog',
         age = 2,
         notes = 'very sweet, likes to play'
     )
 
     bolt = Pet(
-        name = 'Bolt',
-        species = 'bulldog',
+        name = 'Apolo',
+        species = 'Cat',
         age = 1,
-        notes = 'loves to go on walks'
+        notes = 'loves chin scratches'
     )
     cesar = Pet(
         name = 'Cesar',
-        species = 'Lomodo Dragon',
+        species = 'Komodo Dragon',
         age = 2,
         notes = 'super chill'
     )
@@ -43,13 +43,43 @@ if Pet.query.order_by(Pet.name).all() == []:
 
 @app.route('/')
 def root():
-    pets = Pet.query.order_by(Pet.name).all()
+    pets = Pet.query.order_by(Pet.id).all()
     return render_template('index.html', pets=pets)
 
 @app.route('/add', methods=['GET','POST'])
 def add_pet_form():
     form = AddPetForm()
     if form.validate_on_submit():
+        if form.photo_url.data == '':
+            form.photo_url.data = DEFAULT_IMG
+        pet = Pet(
+            name = form.name.data,
+            species = form.species.data,
+            photo_url = form.photo_url.data,
+            age = form.age.data,
+            notes = form.notes.data
+        )
+        db.session.add(pet)
+        db.session.commit()
         return redirect('/')
     else:
         return render_template('add-pet-form.html', form=form)
+
+@app.route('/<int:pet_id>')
+def pet_details(pet_id):
+    pet = Pet.query.get_or_404(pet_id)
+    return render_template('pet_details.html', pet=pet)
+
+@app.route('/<int:pet_id>/edit', methods=['GET', 'POST'])
+def edit_pet(pet_id):
+    pet = Pet.query.get_or_404(pet_id)
+    form = EditPetForm(obj=pet)
+    
+    if form.validate_on_submit():
+        pet.photo_url = form.photo_url.data
+        pet.notes = form.notes.data
+        pet.available = form.available.data
+        db.session.commit()
+        return redirect('/')
+    else:
+        return render_template('/edit-pet-form.html', pet=pet, form=form)
--- forms.py
+++ forms.py
@@ -1,10 +1,15 @@
 from flask_wtf import FlaskForm
-from wtforms import StringField, FloatField
+from wtforms import StringField, FloatField, BooleanField
+from wtforms.validators import NumberRange, AnyOf, URL, Optional
 
 class AddPetForm(FlaskForm):
     name = StringField('Name')
-    species = StringField('Species')
-    img = StringField('Picture')
-    age = FloatField('Age')
+    species = StringField('Species', validators=[AnyOf(['Dog', 'Cat', 'Komodo Dragon'], message='Must be Dog, Cat or Komodo Dragon')])
+    photo_url = StringField('Picture', validators=[Optional(), URL(require_tld=True, message='Must be a Valid URL')])
+    age = FloatField('Age', validators=[NumberRange(min=0, max=30, message='Must be between 0 and 30')])
     notes = StringField('Pet Notes')
-    
+
+class EditPetForm(FlaskForm):
+    photo_url = StringField('Picture', validators=[Optional(), URL(require_tld=True, message='Must be a Valid URL')])
+    notes = StringField('Pet Notes')
+    available = BooleanField('Available')
